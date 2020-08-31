31f6d03ee8af14e5acdce2939e5ed98ab6d40dc2
Tiwari,Harsh <harsh.tiwari@cerner.com>
2020-09-01 01:19:20 +0530

    final code changes

--- liquidbutton.js
+++ liquidbutton.js
@@ -1,294 +1,281 @@
-const LiquidButton = class LiquidButton {
-    constructor(optionsParam) {
-      const options = optionsParam || {};
-      this.tension = options.tension || 0.4;
-      this.width   = options.width   || 200;
-      this.height  = options.width   ||  50;
-      this.margin  = options.margin  ||  50;
-      this.padding = options.padding ||  17;
-      this.hoverFactor = options.hoverFactor || -0.1;
-      this.gap     = options.gap     ||   5;
-      this.debug   = options.debug   || false;
-      this.forceFactor = options.forceFactor || 0.2;
-      this.color1 = options.color1 || '#36DFE7';
-      this.color2 = options.color2 || '#8F17E1';
-      this.color3 = options.color3 || '#E509E6';
-      this.textColor = options.textColor || '#FFFFFF';
-      this.layers = [{
-        points: [],
-        viscosity: 0.5,
-        mouseForce: 100,
-        forceLimit: 2,
-      },{
-        points: [],
-        viscosity: 0.8,
-        mouseForce: 150,
-        forceLimit: 3,
-      }];
-      this.text    = options.text    || 'Click Me';
-      this.canvas  = options.canvas  || document.createElement('canvas');
-      this.context = this.canvas.getContext('2d');
-      this.wrapperElement = options.wrapperElement || document.body;
-      if (!this.canvas.parentElement) {
-        this.wrapperElement.append(this.canvas);
-      }
-      this.touches = [];
-      this.noise = options.noise || 0;
-      this.canvas.addEventListener('mousemove', this.mousemove);
-      this.canvas.addEventListener('mouseout', this.mouseout);
-      this.initOrigins();
-      this.animate();
-    }
-  
-    get mousemove() {
-      return (e) => {
-        this.touches = [{
-          x: e.offsetX,
-          y: e.offsetY,
-          z: 0,
-          force: 1,
-        }];
-      };
-    }
-  
-    get mouseout() {
-      return (e) => {
-        this.touches = [];
-      };
-    }
-  
-    get raf() {
-      return this.__raf || (this.__raf = (
-        window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        function(callback){ setTimeout(callback, 10)}
-      ).bind(window));
-    }
-  
-    distance(p1, p2) {
-      return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
-    }
-  
-    update() {
-      for (let layerIndex = 0; layerIndex < this.layers.length; layerIndex++) {
-        const layer = this.layers[layerIndex];
-        const points = layer.points;
-        for (let pointIndex = 0; pointIndex < points.length; pointIndex++) {
-          const point = points[pointIndex];
-          const dx = point.ox - point.x + (Math.random() - 0.5) * this.noise;
-          const dy = point.oy - point.y + (Math.random() - 0.5) * this.noise;
-          const d = Math.sqrt(dx * dx + dy * dy);
-          const f = d * this.forceFactor;
-          point.vx += f * ((dx / d) || 0);
-          point.vy += f * ((dy / d) || 0);
-          for (let touchIndex = 0; touchIndex < this.touches.length; touchIndex++) {
-            const touch = this.touches[touchIndex];
-            let mouseForce = layer.mouseForce;
-            if (
-              touch.x > this.margin &&
-              touch.x < this.margin + this.width &&
-              touch.y > this.margin &&
-              touch.y < this.margin + this.height
-            ) {
-              mouseForce *= -this.hoverFactor;
-            }
-            const mx = point.x - touch.x;
-            const my = point.y - touch.y;
-            const md = Math.sqrt(mx * mx + my * my);
-            const mf = Math.max(-layer.forceLimit, Math.min(layer.forceLimit, (mouseForce * touch.force) / md));
-            point.vx += mf * ((mx / md) || 0);
-            point.vy += mf * ((my / md) || 0);
-          }
-          point.vx *= layer.viscosity;
-          point.vy *= layer.viscosity;
-          point.x += point.vx;
-          point.y += point.vy;
-        }
-        for (let pointIndex = 0; pointIndex < points.length; pointIndex++) {
-          const prev = points[(pointIndex + points.length - 1) % points.length]; 
-          const point = points[pointIndex];
-          const next = points[(pointIndex + points.length + 1) % points.length];
-          const dPrev = this.distance(point, prev);
-          const dNext = this.distance(point, next);
-  
-          const line = {
-            x: next.x - prev.x,
-            y: next.y - prev.y,
-          };
-          const dLine = Math.sqrt(line.x * line.x + line.y * line.y);
-  
-          point.cPrev = {
-            x: point.x - (line.x / dLine) * dPrev * this.tension,
-            y: point.y - (line.y / dLine) * dPrev * this.tension,
-          };
-          point.cNext = {
-            x: point.x + (line.x / dLine) * dNext * this.tension,
-            y: point.y + (line.y / dLine) * dNext * this.tension,
-          };
-        }
-      }
-    }
-  
-    animate() {
-      this.raf(() => {
-        this.update();
-        this.draw();
-        this.animate();
-      });
-    }
-  
-    draw() {
-      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      for (let layerIndex = 0; layerIndex < this.layers.length; layerIndex++) {
-        const layer = this.layers[layerIndex];
-        if (layerIndex === 1) {
-          if (this.touches.length > 0) {
-            const gx = this.touches[0].x;
-            const gy = this.touches[0].y;
-            layer.color = this.context.createRadialGradient(gx, gy, this.height * 2, gx, gy, 0);
-            layer.color.addColorStop(0, this.color2);
-            layer.color.addColorStop(1, this.color3);
-          } else {
-            layer.color = this.color2;
-          }
-        } else {
-          layer.color = this.color1;
-        }
-        const points = layer.points;
-            this.context.fillStyle = layer.color;
-        
-        this.context.beginPath();
-        this.context.moveTo(points[0].x, points[0].y);
-        for (let pointIndex = 1; pointIndex < points.length; pointIndex += 1) {
-          this.context.bezierCurveTo(
-            points[(pointIndex + 0) % points.length].cNext.x,
-            points[(pointIndex + 0) % points.length].cNext.y,
-            points[(pointIndex + 1) % points.length].cPrev.x,
-            points[(pointIndex + 1) % points.length].cPrev.y,
-            points[(pointIndex + 1) % points.length].x,
-            points[(pointIndex + 1) % points.length].y
-          );
-        }
-        this.context.fill();
-      }
-      this.context.fillStyle = this.textColor;
-      this.context.font = '100 ' + (this.height - this.padding * 2) + 'px sans-serif'; 
-      this.context.textAlign = 'center';
-      this.context.textBaseline = 'middle'; 
-      this.context.fillText(this.text, this.canvas.width / 2, this.canvas.height / 2, this.width - this.padding * 2);
-      if (this.debug) {
-        this.drawDebug();
-      }
-    }
-  
-    drawDebug() {
-      this.context.fillStyle = 'rgba(255, 255, 255, 0.8)';
-      this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
-      for (let layerIndex = 0; layerIndex < this.layers.length; layerIndex++) {
-        const layer = this.layers[layerIndex];
-        const points = layer.points;
-        for (let pointIndex = 0; pointIndex < points.length; pointIndex++) {
-          if (layerIndex === 0) {
-            this.context.fillStyle = this.color1;
-          } else {
-            this.context.fillStyle = this.color2;
-          }
-          const point = points[pointIndex];
-          this.context.fillRect(point.x, point.y, 1, 1);
-          this.context.fillStyle = '#000';
-          this.context.fillRect(point.cPrev.x, point.cPrev.y, 1, 1);
-          this.context.fillRect(point.cNext.x, point.cNext.y, 1, 1);
-          this.context.strokeStyle = 'rgba(0, 0, 0, 0.33)';
-          this.context.strokeWidth = 0.5;
-          this.context.beginPath();
-          this.context.moveTo(point.cPrev.x, point.cPrev.y);
-          this.context.lineTo(point.cNext.x, point.cNext.y);
-          this.context.stroke();
-        }
-      }
-    }
-  
-    createPoint(x, y) {
-      return {
-        x: x,
-        y: y,
-        ox: x,
-        oy: y,
-        vx: 0,
-        vy: 0,
-      };
-    }
-  
-    initOrigins() {
-      this.canvas.width = this.width + this.margin * 2;
-      this.canvas.height = this.height + this.margin * 2;
-      for (let layerIndex = 0; layerIndex < this.layers.length; layerIndex++) {
-        const layer = this.layers[layerIndex];
-        const points = [];
-        for (let x = ~~(this.height / 2); x < this.width - ~~(this.height / 2); x += this.gap) {
-          points.push(this.createPoint(
-            x + this.margin,
-            this.margin
-          ));
-        }
-        for (let alpha = ~~(this.height * 1.25); alpha >= 0; alpha -= this.gap) {
-          const angle = (Math.PI / ~~(this.height * 1.25)) * alpha;
-          points.push(this.createPoint(
-            Math.sin(angle) * this.height / 2 + this.margin + this.width - this.height / 2,
-            Math.cos(angle) * this.height / 2 + this.margin + this.height / 2
-          ));
-        }
-        for (let x = this.width - ~~(this.height / 2) - 1; x >= ~~(this.height / 2); x -= this.gap) {
-          points.push(this.createPoint(
-            x + this.margin,
-            this.margin + this.height
-          ));
-        }
-        for (let alpha = 0; alpha <= ~~(this.height * 1.25); alpha += this.gap) {
-          const angle = (Math.PI / ~~(this.height * 1.25)) * alpha;
-          points.push(this.createPoint(
-            (this.height - Math.sin(angle) * this.height / 2) + this.margin - this.height / 2,
-            Math.cos(angle) * this.height / 2 + this.margin + this.height / 2
-          ));
-        }
-        layer.points = points;
-      }
-    }
-  }
-  
-  
-  const redraw = () => {
-    button.initOrigins();
-  };
-  
-  let button = new LiquidButton();
-  
-  var gui = new dat.GUI();
-  gui.add(button, "debug");
-  gui.add(button, "text");
-  gui.add(button, "gap", 1, 20, 1).onChange(redraw);
-  gui.add(button, "width", 50, 500, 1).onChange(redraw);
-  gui.add(button, "height", 10, 200, 1).onChange(redraw);
-  gui.add(button, "margin", 10, 100, 1).onChange(redraw);
-  gui.add(button, "padding", 0, 30, 1);
-  gui.add(button, "tension", 0, 1, 0.01);
-  gui.add(button, "forceFactor", 0, 1, 0.01);
-  gui.add(button, "hoverFactor", -1, 1, 0.01);
-  gui.addColor(button, "color1");
-  gui.addColor(button, "color2");
-  gui.addColor(button, "color3");
-  gui.addColor(button, "textColor");
-  for (let layerIndex = 0; layerIndex < button.layers.length; layerIndex++) {
-    const layer = button.layers[layerIndex];
-    const folder = gui.addFolder('Layer ' + (layerIndex + 1));
-    folder.add(layer, "viscosity", 0, 1, 0.01);
-    folder.add(layer, "mouseForce", 0, 2000, 1);
-    folder.add(layer, "forceLimit", 0, 200, 0.1);
-    folder.open();
-  }
-  
-  if (window.innerWidth < 1000) {
-    gui.close();
-  }
-  
+$(function() {
+	// Vars
+	var pointsA = [],
+		pointsB = [],
+		$canvas = null,
+		canvas = null,
+		context = null,
+		vars = null,
+		points = 8,
+		viscosity = 20,
+		mouseDist = 70,
+		damping = 0.05,
+		showIndicators = false;
+		mouseX = 0,
+		mouseY = 0,
+		relMouseX = 0,
+		relMouseY = 0,
+		mouseLastX = 0,
+		mouseLastY = 0,
+		mouseDirectionX = 0,
+		mouseDirectionY = 0,
+		mouseSpeedX = 0,
+		mouseSpeedY = 0;
+
+	/**
+	 * Get mouse direction
+	 */
+	function mouseDirection(e) {
+		if (mouseX < e.pageX)
+			mouseDirectionX = 1;
+		else if (mouseX > e.pageX)
+			mouseDirectionX = -1;
+		else
+			mouseDirectionX = 0;
+
+		if (mouseY < e.pageY)
+			mouseDirectionY = 1;
+		else if (mouseY > e.pageY)
+			mouseDirectionY = -1;
+		else
+			mouseDirectionY = 0;
+
+		mouseX = e.pageX;
+		mouseY = e.pageY;
+
+		relMouseX = (mouseX - $canvas.offset().left);
+		relMouseY = (mouseY - $canvas.offset().top);
+	}
+	$(document).on('mousemove', mouseDirection);
+
+	/**
+	 * Get mouse speed
+	 */
+	function mouseSpeed() {
+		mouseSpeedX = mouseX - mouseLastX;
+		mouseSpeedY = mouseY - mouseLastY;
+
+		mouseLastX = mouseX;
+		mouseLastY = mouseY;
+
+		setTimeout(mouseSpeed, 50);
+	}
+	mouseSpeed();
+
+	/**
+	 * Init button
+	 */
+	function initButton() {
+		// Get button
+		var button = $('.btn-liquid');
+		var buttonWidth = button.width();
+		var buttonHeight = button.height();
+
+		// Create canvas
+		$canvas = $('<canvas></canvas>');
+		button.append($canvas);
+
+		canvas = $canvas.get(0);
+		canvas.width = buttonWidth+100;
+		canvas.height = buttonHeight+100;
+		context = canvas.getContext('2d');
+
+		// Add points
+
+		var x = buttonHeight/2;
+		for(var j = 1; j < points; j++) {
+			addPoints((x+((buttonWidth-buttonHeight)/points)*j), 0);
+		}
+		addPoints(buttonWidth-buttonHeight/5, 0);
+		addPoints(buttonWidth+buttonHeight/10, buttonHeight/2);
+		addPoints(buttonWidth-buttonHeight/5, buttonHeight);
+		for(var j = points-1; j > 0; j--) {
+			addPoints((x+((buttonWidth-buttonHeight)/points)*j), buttonHeight);
+		}
+		addPoints(buttonHeight/5, buttonHeight);
+
+		addPoints(-buttonHeight/10, buttonHeight/2);
+		addPoints(buttonHeight/5, 0);
+		// addPoints(x, 0);
+		// addPoints(0, buttonHeight/2);
+
+		// addPoints(0, buttonHeight/2);
+		// addPoints(buttonHeight/4, 0);
+
+		// Start render
+		renderCanvas();
+	}
+
+	/**
+	 * Add points
+	 */
+	function addPoints(x, y) {
+		pointsA.push(new Point(x, y, 1));
+		pointsB.push(new Point(x, y, 2));
+	}
+
+	/**
+	 * Point
+	 */
+	function Point(x, y, level) {
+	  this.x = this.ix = 50+x;
+	  this.y = this.iy = 50+y;
+	  this.vx = 0;
+	  this.vy = 0;
+	  this.cx1 = 0;
+	  this.cy1 = 0;
+	  this.cx2 = 0;
+	  this.cy2 = 0;
+	  this.level = level;
+	}
+
+	Point.prototype.move = function() {
+		this.vx += (this.ix - this.x) / (viscosity*this.level);
+		this.vy += (this.iy - this.y) / (viscosity*this.level);
+
+		var dx = this.ix - relMouseX,
+			dy = this.iy - relMouseY;
+		var relDist = (1-Math.sqrt((dx * dx) + (dy * dy))/mouseDist);
+
+		// Move x
+		if ((mouseDirectionX > 0 && relMouseX > this.x) || (mouseDirectionX < 0 && relMouseX < this.x)) {
+			if (relDist > 0 && relDist < 1) {
+				this.vx = (mouseSpeedX / 4) * relDist;
+			}
+		}
+		this.vx *= (1 - damping);
+		this.x += this.vx;
+
+		// Move y
+		if ((mouseDirectionY > 0 && relMouseY > this.y) || (mouseDirectionY < 0 && relMouseY < this.y)) {
+			if (relDist > 0 && relDist < 1) {
+				this.vy = (mouseSpeedY / 4) * relDist;
+			}
+		}
+		this.vy *= (1 - damping);
+		this.y += this.vy;
+	};
+
+
+	/**
+	 * Render canvas
+	 */
+	function renderCanvas() {
+		// rAF
+		rafID = requestAnimationFrame(renderCanvas);
+
+		// Clear scene
+		context.clearRect(0, 0, $canvas.width(), $canvas.height());
+		context.fillStyle = '#ff505000';
+		context.fillRect(0, 0, $canvas.width(), $canvas.height());
+
+		// Move points
+		for (var i = 0; i <= pointsA.length - 1; i++) {
+			pointsA[i].move();
+			pointsB[i].move();
+		}
+
+		// Create dynamic gradient
+		var gradientX = Math.min(Math.max(mouseX - $canvas.offset().left, 0), $canvas.width());
+		var gradientY = Math.min(Math.max(mouseY - $canvas.offset().top, 0), $canvas.height());
+		var distance = Math.sqrt(Math.pow(gradientX - $canvas.width()/2, 2) + Math.pow(gradientY - $canvas.height()/2, 2)) / Math.sqrt(Math.pow($canvas.width()/2, 2) + Math.pow($canvas.height()/2, 2));
+
+		var gradient = context.createRadialGradient(gradientX, gradientY, 300+(300*distance), gradientX, gradientY, 0);
+		gradient.addColorStop(0, '#102ce5');
+		gradient.addColorStop(1, '#E406D6');
+
+		// Draw shapes
+		var groups = [pointsA, pointsB]
+
+		for (var j = 0; j <= 1; j++) {
+			var points = groups[j];
+
+			if (j == 0) {
+				// Background style
+				context.fillStyle = '#1CE2D8';
+			} else {
+				// Foreground style
+				context.fillStyle = gradient;
+			}
+
+			context.beginPath();
+			context.moveTo(points[0].x, points[0].y);
+
+			for (var i = 0; i < points.length; i++) {
+				var p = points[i];
+				var nextP = points[i + 1];
+				var val = 30*0.552284749831;
+
+				if (nextP != undefined) {
+					// if (nextP.ix > p.ix && nextP.iy < p.iy) {
+					// 	p.cx1 = p.x;
+					// 	p.cy1 = p.y-val;
+					// 	p.cx2 = nextP.x-val;
+					// 	p.cy2 = nextP.y;
+					// } else if (nextP.ix > p.ix && nextP.iy > p.iy) {
+					// 	p.cx1 = p.x+val;
+					// 	p.cy1 = p.y;
+					// 	p.cx2 = nextP.x;
+					// 	p.cy2 = nextP.y-val;
+					// }  else if (nextP.ix < p.ix && nextP.iy > p.iy) {
+					// 	p.cx1 = p.x;
+					// 	p.cy1 = p.y+val;
+					// 	p.cx2 = nextP.x+val;
+					// 	p.cy2 = nextP.y;
+					// } else if (nextP.ix < p.ix && nextP.iy < p.iy) {
+					// 	p.cx1 = p.x-val;
+					// 	p.cy1 = p.y;
+					// 	p.cx2 = nextP.x;
+					// 	p.cy2 = nextP.y+val;
+					// } else {
+
+						p.cx1 = (p.x+nextP.x)/2;
+						p.cy1 = (p.y+nextP.y)/2;
+						p.cx2 = (p.x+nextP.x)/2;
+ 						p.cy2 = (p.y+nextP.y)/2;
+
+						context.bezierCurveTo(p.x, p.y, p.cx1, p.cy1, p.cx1, p.cy1);
+					// 	continue;
+					// }
+
+					// context.bezierCurveTo(p.cx1, p.cy1, p.cx2, p.cy2, nextP.x, nextP.y);
+				} else {
+nextP = points[0];
+						p.cx1 = (p.x+nextP.x)/2;
+						p.cy1 = (p.y+nextP.y)/2;
+
+						context.bezierCurveTo(p.x, p.y, p.cx1, p.cy1, p.cx1, p.cy1);
+				}
+			}
+
+			// context.closePath();
+			context.fill();
+		}
+
+		if (showIndicators) {
+			// Draw points
+			context.fillStyle = '#000';
+			context.beginPath();
+			for (var i = 0; i < pointsA.length; i++) {
+				var p = pointsA[i];
+
+				context.rect(p.x - 1, p.y - 1, 2, 2);
+			}
+			context.fill();
+
+			// Draw controls
+			context.fillStyle = '#f00';
+			context.beginPath();
+			for (var i = 0; i < pointsA.length; i++) {
+				var p = pointsA[i];
+
+				context.rect(p.cx1 - 1, p.cy1 - 1, 2, 2);
+				context.rect(p.cx2 - 1, p.cy2 - 1, 2, 2);
+			}
+			context.fill();
+		}
+	}
+
+	// Init
+	initButton();
+});
